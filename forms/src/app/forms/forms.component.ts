51f31087443dbaa7c9af2e73cd401f0ec08f37cd
Josué Medino <josuemedino5@gmail.com>
2023-10-07 10:56:17 -0300

    first commit

---
+++ forms/src/app/forms/forms.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { User } from '../User.interface';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.css']
+})
+export class FormsComponent implements User   {
+  name: string = ''
+  email:string = ''
+  password:string = ''
+  bool: boolean = false;
+
+
+
+  submit(login:any){
+    this.name = login.value.name;
+    this.email = login.value.email;
+    this.password = login.value.password;
+
+    
+
+    console.log(this.name, this.email, this.password)
+  }
+
+
+}
